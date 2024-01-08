47e151552937ef7b6570071a568f3e9e22d6dd69
Fabio Guardado <fabio.guardado2012@gmail.com>
2024-01-07 18:06:52 -0600

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
-const App = () => {
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import ROUTES from './constants/routes';
+
+import { HomePage } from './pages/HomePage';
+import { Dashboard } from './pages/Dashboard';
+import { NotFound } from './pages/NotFound';
+
+const { HOMEPAGE, DASHBOARD } = ROUTES;
+
+const App = () => {
   return (
-    <h1>Hello React</h1>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path={DASHBOARD} element={<Dashboard />}></Route>
+        <Route path={HOMEPAGE} element={<HomePage />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
